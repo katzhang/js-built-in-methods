e5cfabef78e093cbe16e4916e5c39bc402364cf2
Kat Zhang <arcadychang@gmail.com>
2014-08-06 15:16:33 -0400

    adding array native methods

--- app/scripts/array.js
+++ app/scripts/array.js
@@ -56,8 +56,8 @@ function concatReverse(array) {
 /**
  * Array.prototype.every()
  *
- * @param {callback(index, element, array)[, thisArg]} Callback function to be executed on every element
- * @returns {boolean} Returns false as long as one of the elements fails the callback test. Otherwise returns true.
+ * @param {callback(value, index, array)[, thisArg]} Callback function to be executed on every element
+ * @returns {boolean} Returns false as long as one of the elements fails the callback test. Otherwise returns true
  */
  function useEvery() {
  	function largerThan10(el, i, array) {
@@ -66,4 +66,65 @@ function concatReverse(array) {
 
  	console.log([1, 2, 3].every(largerThan10)); //Outputs false;
  	console.log([10, 20, 30].every(largerThan10)); //Outputs true;
+ }
+
+ /**
+ * Array.prototype.filter()
+ *
+ * @param {callback(value, index, array)[, thisArg]} Callback function to be executed on every element
+ * @returns {array} Returns a new array of all the values that pass the callback function (return true)
+ */
+ function useFilter() {
+ 	function largerThan10(el, i, array) {
+ 		return (el >= 10);
+ 	}
+
+ 	console.log([1, 2, 3].filter(largerThan10)); //Outputs [].
+ 	console.log([1, 2, 30].filter(largerThan10)); //Outputs [30].
+ }
+
+ /**
+ * Array.prototype.forEach()
+ *
+ * @param {callback(value, index, array)[, thisArg]} Callback function to be executed on every element. No way to stop or break the loop.
+ * @returns {undefined}
+ */
+ function useForEach() {
+ 	function addOne(el, i, array) {
+ 		array[i] = el+1;
+ 	}
+
+ 	console.log([1, 2, 3].forEach(addOne)); //Outputs undefined. But the original array is now [2, 3, 4].  
+ }
+
+ /**
+ * Array.prototype.indexOf()
+ *
+ * @param {value[, index]} Pass in the value you want to search and optionally the index to start from.
+ * @returns {index} Returns the first index at which a given element can be found, or -1 if it is not present.
+ */
+ function useIndexOf() {
+ 	var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+
+ 	console.log(array.indexOf('a')); //Outputs 0.
+ 	console.log(array.indexOf('a', 1)); //Outputs 3.
+ 	console.log(array.indexOf('a', -1)); //Outputs -1. When negative index is passed in, the start position will be array.length + negative index.
+ 	console.log(array.indexOf('a', -3)); //Outputs 3.
+ 	console.log(array.indexOf('a', -10)); //Outputs 0. When even the calculated index is negative, returns 0.
+
+ }
+
+ /**
+ * Array.prototype.join()
+ *
+ * @param {separator} By default if omitted it is a comma.
+ * @returns {string} Returns string separated by separator.
+ */
+ function useJoin() {
+ 	var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+
+ 	console.log(array.join('')); //Outputs 'abcabc'.
+ 	console.log(array.join(' ')); //Outputs 'a b c a b c'.
+ 	console.log(array.join()); //Outputs 'a,b,c,a,b,c'.
+
  }
