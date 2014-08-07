d163eaa0713be4b813dbed20cb9441ef5df4f021
Kat Zhang <arcadychang@gmail.com>
2014-08-07 16:07:21 -0400

    added string native methods

--- app/scripts/string.js
+++ app/scripts/string.js
@@ -65,8 +65,74 @@ console.log('abc'.concat('d', 'e')); //Outputs "abcde"
  * String.prototype.match(regex)
  *
  * @param {regex} A regular expression to search.
- * @returns {array} Returns an array of all the matched results. Null if none matches.
+ * @returns {array} Returns an array of the matched results. Null if none matches.
  */
+console.log('abcabc'.match(/a/)); //Outputs ["a"]
+
+/**
+ * String.prototype.replace(regexp|substr, newSubStr|function[,   flags]);
+ *
+ * @param {regex | string} A pattern or a substring to be replaced.
+ * @param {string | function} A new substring to replace or a function that is executed on the old string and return a new substring.
+ * @returns {string} Returns a new string.
+ */
+console.log('abcabc'.replace(/a/, 'n')); //Outputs "nbcabc"
+
+/**
+ * String.prototype.search(regex)
+ *
+ * @param {regex} A regular expression to search for.
+ * @returns {index} Returns the index of the first match of the regular expression inside the string. Otherwise, it returns -1.
+ */
+console.log('abcabc'.search(/a/)); //Outputs 0
+console.log('abcabc'.search(/e/)); //Outputs -1
+
+
+
+
+
+
+
+
+/**
+ * String.prototype.link(url)
+ *
+ * @param {string} The URL as the anchor's href attribute.
+ * @returns {string} Returns the HTML snippet.
+ */
+console.log('this is a google link'.link('http://www.google.com')); //Outputs "<a href="http://www.google.com">this is a google link</a>"
+
+
+
+
+
+ /**
+ * String.prototype.slice(beginIndex[, endIndex])
+ *
+ * @param {index} Pass in the index where to begin extraction and optionally the index to end extraction (not including).
+ * @returns {string} Returns the sliced string.
+ */
+console.log('abcd'.slice(1, 2)); //Outputs "b"
+
+ /**
+ * String.prototype.substring(beginIndex[, endIndex])
+ *
+ * @param {index} Pass in the index where to begin extraction and optionally the index to end extraction (not including).
+ * @returns {string} Returns the sliced string.
+ */
+console.log('abcd'.substring(1, 2)); //Outputs "b"
+console.log('abcd'.substring(2, 1)); //Outputs "b"
+
+ /**
+ * String.prototype.substr(beginIndex[, length])
+ *
+ * @param {index} Pass in the index where to begin extraction and optionally the length (number of characters).
+ * @returns {string} Returns the sub string.
+ */
+console.log('abcde'.substr(1)); //Outputs "bcde"
+console.log('abcde'.substr(-1)); //Outputs "e"
+console.log('abcde'.substr(1,3)); //Outputs "bcd"
+console.log('abcde'.substr(1,-3)); //Outputs ""
 
 
 
@@ -78,6 +144,36 @@ console.log('abc'.concat('d', 'e')); //Outputs "abcde"
 
 
 
+ /**
+ * String.prototype.split(separator) (counterpart of Array.prototype.join())
+ *
+ * @param {separator} Pass in the index where to begin extraction and optionally the index to end extraction (not including).
+ * @returns {array} Returns the sliced string.
+ */
+console.log('abc'.split()); //Outputs ["abc"]
+console.log('abc'.split('')); //Outputs ["a", "b", "c"]
+console.log('a,b,c'.split(',')); //Outputs ["a", "b", "c"]
+
+ /**
+ * String.prototype.toLowerCase()
+ *
+ * @returns {string} Returns the old string converted to lowercase characters.
+ */
+console.log('AbC'.toLowerCase()); //Outputs "abc"
+
+ /**
+ * String.prototype.toUpperCase()
+ *
+ * @returns {string} Returns the old string converted to lowercase characters.
+ */
+console.log('AbC'.toUpperCase()); //Outputs "ABC"
+
+ /**
+ * String.prototype.trim()
+ *
+ * @returns {string} Returns the string. White space removed from both ends.
+ */
+console.log('AbC'.toUpperCase()); //Outputs "ABC"
 
 
 
