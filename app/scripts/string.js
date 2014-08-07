7249669af840ddf7bc5f33157365b6ae15d0977f
Kat Zhang <arcadychang@gmail.com>
2014-08-07 09:56:47 -0400

    added string js

---
+++ app/scripts/string.js
@@ -0,0 +1,6 @@
+/**
+ * String.fromCharCode()
+ *
+ * @param {number1, number2, ...numbern} A sequence of numbers that are Unicode values.
+ * @returns {string} Returns a string but not String object.
+ */
